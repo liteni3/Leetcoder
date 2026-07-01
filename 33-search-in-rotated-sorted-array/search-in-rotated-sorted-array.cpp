dcf741d4845db8cc0cafac23b24b855ba6fc4bb5
nitin492000 <103478797+liteni3@users.noreply.github.com>
2026-07-01 15:48:09 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (34.52%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        //nitin
-        int ans;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                ans=i;
-                break;
+        int s=0,e=nums.size()-1,mid,ans;
+        while(s<=e){
+            mid=(s+e)/2;
+             //check if right array or left array which is sorted and then check for the condition on which part the target lies
+            //if left part is sorted 
+            if(nums[mid]==target)return mid;
+            if(nums[mid]>=nums[s]){
+                //now we will check if this target lies in this sorted array or not
+                if(target>=nums[s] && target <nums[mid]){
+                    e=mid-1;
+                }else {
+                    s=mid+1;
+                }
             }
-            ans=-1;
+            //if right part is sorted 
+            else {
+             if(target>nums[mid] && target <=nums[e]){
+                  s=mid+1;
+             }else {
+                e=mid-1;
+             }
+            }
         }
-        return ans;
+         return -1;
     }
 };
