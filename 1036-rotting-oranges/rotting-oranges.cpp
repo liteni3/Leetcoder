f26f151ec168948058456f0c2488577f595bf644
nitin492000 <103478797+liteni3@users.noreply.github.com>
2026-08-13 19:58:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (16.82%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         queue<pair<pair<int,int>,int>> q;
-        int r=grid.size(); // number of rows
-        int c=grid[0].size(); //number of colomns
-        int vis[r][c];
+        int r=grid.size();
+        int c=grid[0].size();
+        int tm=0;
+        vector<vector<int>> vis(r,vector<int>(c,-1));
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(grid[i][j]==2){
                     q.push({{i,j},0});
                     vis[i][j]=2;
-                }else vis[i][j]=0;
+                }
             }
         }
-
-        //bfs
-        // (-1,0) , (1,0) , (0,1) , (0,-1)
         int row[]={-1,1,0,0};
         int col[]={0,0,1,-1};
-        int tm=0;
         while(!q.empty()){
             int r1=q.front().first.first;
             int c1=q.front().first.second;
@@ -27,20 +24,17 @@ public:
             for(int i=0;i<4;i++){
                 int r2=r1+row[i];
                 int c2=c1+col[i];
-                if(r2>=0 && r2<r && c2>=0 && c2<c && grid[r2][c2]==1 && vis[r2][c2]!=2){
+                if(r2>=0 && r2<r && c2>=0 && c2<c && grid[r2][c2]==1 && vis[r2][c2]==-1){
                     q.push({{r2,c2},tm+1});
                     vis[r2][c2]=2;
                 }
             }
-
         }
-
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(vis[i][j]==0 && grid[i][j]==1)return -1;
+                if(grid[i][j]==1 && vis[i][j]==-1)return -1;
             }
         }
         return tm;
-
     }
 };
