74b715ea8920f552d7b60d362eec8574d506f6c9
nitin492000 <103478797+liteni3@users.noreply.github.com>
2026-08-15 16:34:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (47.19%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -5,8 +5,7 @@ public:
         if(i>=nums.size()) return 0;
         //take this house then if taking only can take i+2
         if(dp[i]!=-1)return dp[i];
-        dp[i]= max (nums[i] + f(nums,i+2,dp),f(nums,i+1,dp));
-        return dp[i];
+        return dp[i]= max (nums[i] + f(nums,i+2,dp),f(nums,i+1,dp));
 
     }
     int rob(vector<int>& nums) {
