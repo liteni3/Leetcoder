0748d9d7fd872e0b88e3b235107a2840df5ee413
nitin492000 <103478797+liteni3@users.noreply.github.com>
2026-08-17 00:17:07 +0530

    Time: 27 ms (55.87%) | Memory: 16.3 MB (80.83%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid,int i,int j){
+            if(i>=grid.size() || i<0 || j>=grid[0].size() || j<0 ||grid[i][j] == '0' )return;
+            if(grid[i][j]=='1')grid[i][j]='0';
+            dfs(grid,i+1,j);
+            dfs(grid,i-1,j);
+            dfs(grid,i,j+1);
+            dfs(grid,i,j-1);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1'){
+                    dfs(grid,i,j);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
