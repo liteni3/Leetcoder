3c4123786c56d844788d0aee4719d41e6f96b19f
nitin492000 <103478797+liteni3@users.noreply.github.com>
2026-08-17 13:37:38 +0530

    Time: 92 ms (5.86%) | Memory: 72.3 MB (5.34%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    void f(vector<int>& candidates,int target,vector<vector<int>> &ans,vector<int> &v,int ref,int i){
-        //base 
-        if(ref > target || i >= candidates.size()) return;
-        if(ref==target){
-            ans.push_back(v);
-            return;
-        }
-        //take
-        v.push_back(candidates[i]);
-        f(candidates,target,ans,v,ref+candidates[i],i);
-        v.pop_back();
-        f(candidates,target,ans,v,ref,i+1);
+    void f(int i,vector<vector<int>> &ans,vector<int>& nums,int target,vector<int> ref,int cnt){
+        if(cnt > target || i >= nums.size()) return;
+            if(cnt==target){
+                ans.push_back(ref);
+                return ;
+            }
+        //pick 
+        ref.push_back(nums[i]);
+        f(i,ans,nums,target,ref,cnt+nums[i]);
+        ref.pop_back();
+        f(i+1,ans,nums,target,ref,cnt);
 
-        
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        // any number of times - each element can be picked any number of times 
         vector<vector<int>> ans;
-        vector<int> v;
-        int ref=0;
-        f(candidates,target,ans,v,ref,0);
+        vector<int> ref;
+        int cnt=0;
+        f(0,ans,candidates,target,ref,cnt);
         return ans;
-        
     }
 };
