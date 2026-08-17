482f617c01dc4a7457859cfc88cb8fc9f5c26fc4
nitin492000 <103478797+liteni3@users.noreply.github.com>
2026-08-17 12:33:02 +0530

    Time: 929 ms (5.24%) | Memory: 417.1 MB (5.50%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,57 +1,37 @@
 class Solution {
 public:
-
-    bool f(int src, int target, vector<int> adj[], int n) {
-
+    bool f(int src, int target,vector<vector<int>> edges, vector<int> adj[] ){
         queue<int> q;
-        vector<int> vis(n + 1, 0);
-
+        vector<int> vis(edges.size()+1,-1); // n+1 size of visited array because the edges starts from 1 number 
         q.push(src);
-        vis[src] = 1;
-
-        while (!q.empty()) {
-
-            int node = q.front();
+        vis[src]=1; 
+        while(!q.empty()){
+            int ref= q.front();
             q.pop();
-
-            if (node == target) {
-                return true;
-            }
-
-            for (auto adjNode : adj[node]) {
-
-                if (!vis[adjNode]) {
-                    vis[adjNode] = 1;
-                    q.push(adjNode);
+            if(ref==target)return true;
+            for(auto it : adj[ref]){
+                if(vis[it]==-1){
+                q.push(it);
+                vis[it]=1;
                 }
             }
         }
-
         return false;
     }
-
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-
-        int n = edges.size();
-
-        // +1 because nodes are numbered from 1 to n
+        // The idea is start building the graph and check every time while iterating through edges if there is any path that exists between source and target where src= 0th endex and target = 1st index of every edge 
+        int n=edges.size();
         vector<int> adj[n + 1];
-
-        for (auto it : edges) {
-
-            int src = it[0];
-            int target = it[1];
-
-            // First check whether a path already exists
-            if (f(src, target, adj, n)) {
-                return {src, target};
-            }
-
-            // If no path exists, add this edge
+        for(int i=0;i<edges.size();i++){
+            int src= edges[i][0];
+            int target= edges[i][1];
+            if(f(src,target,edges,adj)== true){
+                return {src,target};
+          }else {
             adj[src].push_back(target);
             adj[target].push_back(src);
+          }
         }
-
-        return {};
+        return {-1,-1};
     }
 };
