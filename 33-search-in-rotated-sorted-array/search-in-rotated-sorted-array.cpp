ddeab13cc672f6a9ba48381f449f2105c0e34513
nitin492000 <103478797+liteni3@users.noreply.github.com>
2026-08-20 09:33:57 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (96.79%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -10,7 +10,7 @@ public:
             if(nums[mid]==target)return mid;
             //if left is sorted ?
             if(nums[mid]>=nums[s]){
-                if(nums[mid]>=target && target >=nums[s]){
+                if(nums[mid]>target && target >=nums[s]){
                     e=mid-1;
                 }else {
                     s=mid+1;
@@ -18,7 +18,7 @@ public:
             }
             // right is sorted ?
             else if(nums[mid]<=nums[e]) {
-                if(target >=nums[mid] && target <=nums[e]){
+                if(target >nums[mid] && target <=nums[e]){
                     s=mid+1;
                 }else {
                     e=mid-1;
