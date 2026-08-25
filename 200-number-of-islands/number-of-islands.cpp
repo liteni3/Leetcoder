737be8d2793bfcd6e5f88ee047944fc9593c6736
nitin492000 <103478797+liteni3@users.noreply.github.com>
2026-08-25 15:15:13 +0530

    Time: 29 ms (41.43%) | Memory: 23.3 MB (28.97%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid,int i,int j){
-            if(i>=grid.size() || i<0 || j>=grid[0].size() || j<0 ||grid[i][j] == '0' )return;
-            if(grid[i][j]=='1')grid[i][j]='0';
-            dfs(grid,i+1,j);
-            dfs(grid,i-1,j);
-            dfs(grid,i,j+1);
-            dfs(grid,i,j-1);
-    }
-    int numIslands(vector<vector<char>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        int ans=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='1'){
-                    dfs(grid,i,j);
-                    ans++;
+    void bfs(int i,int j,vector<vector<char>>& grid){
+        queue<pair<int,int>> q;
+        int row[]={1,-1,0,0};
+        int col[]={0,0,1,-1};
+        grid[i][j]='0';
+        q.push({i,j});
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            for(int k=0;k<4;k++){
+                int r1=r+row[k];
+                int c1=c+col[
+                    k];
+                if(r1>=0 && r1<grid.size() && c1>=0 && c1<grid[0].size() && grid[r1][c1]=='1'){
+                    q.push({r1,c1});
+                    grid[r1][c1]='0';
                 }
             }
         }
-        return ans;
+
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        //BFS
+        // iterate through whole grid using bfs and keep on updating grid[i][j]==1 then grid[i][j]='0'
+        int cnt=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]=='1'){
+                    cnt++;
+                    bfs(i,j,grid);
+                }
+            }
+        }
+        return cnt;
     }
 };
-
